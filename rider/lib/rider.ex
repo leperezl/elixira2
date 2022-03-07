745ea430c0d3fa7e8080c8dc9792f54b99ea85fa
Luis Perez <le.perezl@uniandes.edu.co>
2022-03-07 10:53:51 -0500

    ohh yeahhh random constt

--- rider/lib/rider.ex
+++ rider/lib/rider.ex
@@ -1,26 +1,9 @@
-defmodule Rider do
-  @rider Rider
-
-    children = [
-    Mutex.child_spec(@rider)
-    ]
-    {:ok, _pid} = Supervisor.start_link(children, strategy: :one_for_one)
-
-  #Idea:  Even bigger Agent that saves the settings of the driver, then its function starts the req agent
-  #Idea:  My idea is to have a Grand Agent that saves the chosen apps and then starts the req agent
-  #      -Now when the user selects a service request (turns it into a service), the ServiceRun starts
-
-  #Doubt:  So i will need to pass the state of a child agent to the parent, so that i can start one or toggle
-  #        - Reqs and Services
-  #Doubt: Can you have a tuple as the state of the agent, or is it ok to have a list
-  #     - I would like to give the ServiceReq agent a list/tuple of all the chosen services
-  #     - So that when the reqs start_link they know what reqs to begin sending indefinitely
 
   # Services  (Uber, Didi, Beat, Indriver, Cabify, Taxi, Uber eats, iFood, Didi Food) 
             # Id: :Name constant      Comm: API  (no idea)
   # Service Req
             # When registered ^Service  For each, services should be generated. Have pattern matching and spawn generator or List Iterator
-            # Id: "Service+Count"  Username: "String"   Type: :Food :Drive     Price: Random Int        Payment: 
+            # Id: "Service+Count"  Username: "String"   Type: :Food :Drive     Price: Random Int        Payment:        TimeSent:
             # For each ^Service, each service has Type and 3 possible payment methods (we can use just one)
   #Chosen Ride
             # When User selects "Service+Count" ex: "Uber5" (Doubt) -> i have no idea how to select those
@@ -30,7 +13,26 @@ defmodule Rider do
             #->  When Selected the spawns shouldnt keep sending stuff. Bigger Mutex that is released
             #-> Bigger Mutex activated by global boolean that will signify one request is activated and it should be changed after a timeout
             #-> 2 functions, one to select init and one that will return after X time
-  def rider do
-    :world
-  end
+defmodule Rider do
+  @menu MenuMutex
+
+
+    @rider __MODULE__
+    @payType {:food, :drive}
+    @uber %{:id=> "uber" , :comm=> "api"}
+    @didi %{:id=> "didi" , :comm=> "api"}
+    @beat %{:id=> "beat" , :comm=> "api"}
+    @taxi %{:id=> "taxi" , :comm=> "phone"}
+
+    def payType do @payType end
+    def randType(n) do
+        elem(@payType, n)
+    end
+    def start_link do
+      children = [
+      Mutex.child_spec(@menu)
+      ]
+      {:ok, _pid} = Supervisor.start_link(children, strategy: :one_for_one)
+        Agent.start_link(fn ->%{} end , @rider)
+    end
 end
