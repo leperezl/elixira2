7bef9cea12df8814181b359f7dceff79e8b3762e
Luis Perez <le.perezl@uniandes.edu.co>
2022-03-07 15:17:24 -0500

    randomizers

--- rider/lib/rider.ex
+++ rider/lib/rider.ex
@@ -15,19 +15,37 @@
             #-> 2 functions, one to select init and one that will return after X time
 defmodule Rider do
   @menu MenuMutex
-
-
-    @rider __MODULE__
-    @payType {:food, :drive}
-    @uber %{:id=> "uber" , :comm=> "api"}
-    @didi %{:id=> "didi" , :comm=> "api"}
-    @beat %{:id=> "beat" , :comm=> "api"}
-    @taxi %{:id=> "taxi" , :comm=> "phone"}
+  @rider __MODULE__
+  @rideType {:food, :drive}
+  @comm {:api, :phone, :mssg, :fax}
+  @payType {:cash, :visa, :mastercard, :points, :epay}
 
     def payType do @payType end
-    def randType(n) do
-        elem(@payType, n)
+    def comm do @comm end
+    def rideType do @rideType end
+
+    def randPayType() do
+      elem(@payType,:rand.uniform(5) -1)
     end
+   
+    def randComm() do
+      elem(@comm,:rand.uniform(4) -1)
+    end
+   
+    def randRideType() do
+      elem(@rideType,:rand.uniform(2) -1)
+    end
+    
+    def randRideTime() do
+      r =:rand.uniform(90)
+      "#{r} minutes"
+    end
+
+    def randLocation() do
+      r =:rand.uniform(90)/17.665
+      "Coordinates: #{r}"
+    end
+    
     def start_link do
       children = [
       Mutex.child_spec(@menu)
@@ -35,4 +53,5 @@ defmodule Rider do
       {:ok, _pid} = Supervisor.start_link(children, strategy: :one_for_one)
         Agent.start_link(fn ->%{} end , @rider)
     end
+    
 end
